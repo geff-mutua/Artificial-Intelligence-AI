1ff4a8508f3020daec632af2385ec2b3db959a1b
geff-mutua <geoffreymutua01@gmail.com>
2023-04-14 13:04:36 +0300

    Added Url for server

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async(e) =>{
   loader(messageDiv);
 
   // Fetch response from the server
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://gideon.onrender.com/',{
     method: 'POST',
     headers:{
       'Content-Type': 'Application/json'
